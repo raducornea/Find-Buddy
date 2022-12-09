de097965e3412bfaf73e726189a2f51d22c102a1
Radu-Valentin Cornea <radu-valentin.cornea@student.tuiasi.ro>
2022-12-09 17:33:50 +0200

    Changed email constraint to be Unique as well

--- Scripts/Create/CreateUsers.sql
+++ Scripts/Create/CreateUsers.sql
@@ -2,7 +2,7 @@ create table users(
 	id int auto_increment,
 	username varchar(30) not null unique,
 	password varchar(100) not null,
-	email varchar(150) not null,
+	email varchar(150) not null unique,
 	first_name varchar(35) not null,
 	last_name varchar(35) not null,
 	primary key(id)
