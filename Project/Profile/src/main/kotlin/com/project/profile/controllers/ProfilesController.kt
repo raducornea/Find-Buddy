30e05b1e867f1f07e1e618d7a9fabdb81781a89a
raducornea <cornearaduvalentin@gmail.com>
2023-04-17 19:43:29 +0300

    Can search users by their first name / last name - Backend

--- Project/Profile/src/main/kotlin/com/project/profile/controllers/ProfilesController.kt
+++ Project/Profile/src/main/kotlin/com/project/profile/controllers/ProfilesController.kt
@@ -3,10 +3,13 @@ package com.project.profile.controllers
 import com.project.profile.data.dtos.UserDTO
 import com.project.profile.data.entities.UserProfile
 import com.project.profile.persistence.repositories.ProfileRepository
+import org.apache.coyote.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import kotlin.collections.HashMap
 
 @RestController
 @CrossOrigin
@@ -44,5 +47,19 @@ class ProfilesController {
         response["available"] = isAvailable
 
         return ResponseEntity.ok(response)
+    }
+	
+    @GetMapping("/users")
+    fun filterAllUsers(
+        @RequestParam(name = "search", defaultValue = "", required = false) search: Optional<String>,
+    ): ResponseEntity<Any> {
+
+
+        if (search.isPresent && search.get() != "") {
+            val optionalUsers = profileRepository.findUsersNotMyIDAndMatchingName(21, search.get())
+            return ResponseEntity.ok().body(optionalUsers)
+        }
+
+        return ResponseEntity.ok().body("")
     }
 }
